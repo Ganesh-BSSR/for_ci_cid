044143e39aa2b0259f49a03383721780f1838e42
Ganesh-BSSR <145440864+Ganesh-BSSR@users.noreply.github.com>
2023-09-27 16:50:28 +0530

    Configured Mocha test environment and added a sample test file

---
+++ app.test.js
@@ -0,0 +1,46 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const mocha = require('mocha')
+const app = require('./index')
+chai.use(chaiHttp)
+const expect = chai.expect
+
+describe('User API', () => {
+  describe('POST /login', () => {
+    it('Should handle user login', (done) => {
+      chai
+        .request(app)
+        .post('/login')
+        .send({ username: 'Ganesh', password: 'Ganesh' })
+        .end((err, res) => {
+          if (err) {
+            expect(res).to.have.status(400)
+          } else {
+            expect(res).to.have.status(200)
+            expect(res.body)
+              .to.have.property('message')
+              .equal('Logged in Successfully!')
+          }
+          done()
+        })
+    })
+
+    it('Should handle user failed login', (done) => {
+      chai
+        .request(app)
+        .post('/login')
+        .send({ username: 'Ganesh', password: 'Ganes' })
+        .end((err, res) => {
+          if (err) {
+            expect(res).to.have.status(400)
+          } else {
+            expect(res).to.have.status(200)
+            expect(res.body)
+              .to.have.property('message')
+              .equal('Logged in Successfully!')
+          }
+          done()
+        })
+    })
+  })
+})
--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+app.post('/login', (req, res) => {
+  const body = req.body
+  const { username, password } = body
+  if (username === 'Ganesh' && password === 'Ganesh') {
+    res.status(200).send({ message: 'Logged in Successfully!' })
+  } else {
+    res.status(400).send('Login Failed')
+  }
+})
+
 app.listen(3000, () => {
   console.log('Server started at port: 3000')
 })
+
+module.exports = app
